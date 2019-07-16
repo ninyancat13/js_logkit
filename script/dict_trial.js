6be442b933364011ba65c15bff8c1ea80d8b9922
ninyancat13 <13ninak@gmail.com>
2019-07-16 12:41:56 +0800

    Removed trial folder

--- script/dict_trial.js
+++
@@ -1,8 +0,0 @@
-function isDict(v) {
-  return typeof v==='object' && v!==null && !(v instanceof Array) && !(v instanceof Date);
-}
-
-value = isDict({"schmekles": "[1,2,3]"});
-value2 = isDict("meow");
-console.log(value);
-console.log(value2);
