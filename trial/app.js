61afc0ab07699890559af43e1c2bffbac90662b0
ninyancat13 <13ninak@gmail.com>
2019-07-16 12:16:31 +0800

    Remove trial folder

--- trial/app.js
+++
@@ -1,6 +0,0 @@
-// app.js
-// ======
-var tools = require('./tools');
-console.log(typeof tools.foo); // => 'function'
-console.log(typeof tools.bar); // => 'function'
-console.log(typeof tools.zemba); // => undefined
